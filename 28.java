ba08c9a00077b571f179cb09083c0c92c5f9b866
Yashika <yashikamandaogade@gmail.com>
2026-03-31 15:17:20 +0530

    TextProcessingBuilder

---
+++ 28.java
@@ -0,0 +1,7 @@
+class TextProcessingBuilder {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Hello");
+        sb.append(" Builder");
+        System.out.println("Modified String: " + sb);
+    }
+}
