23e48aa8bb9e59e18c869c30deaf6942e28c5c9a
Yashika <yashikamandaogade@gmail.com>
2026-03-31 15:20:24 +0530

    DivisionCalculator

---
+++ 32.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class DivisionCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println("Result: "+(a/b));
+        } catch(ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        }
+    }
+}
